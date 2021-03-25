905f370fc239aa04b10a914a4a5833928659bb44
duynguyen2019 <duynguyen1993@csu.fullerton.edu>
2021-03-25 11:35:17 -0700

    Test the repository

--- GeneralDiffFunction_complete.py
+++ GeneralDiffFunction_complete.py
@@ -6,62 +6,81 @@ import re
 
 
 
-eng = create_engine("postgresql://smcread:1969$Harbor@192.168.1.17:5432/smc")
-database_data = pd.read_sql("SELECT * FROM tbl_siteeval WHERE login_agency = 'VCWPD' AND created_date = '2019-08-13 09:38:49' ",eng)
-user_data = pd.read_excel("C:\\Users\\GisUser\\Desktop\\user_data.xls")
-user_data.columns = map(str.lower, user_data.columns)
+# eng = create_engine("postgresql://smcread:1969$Harbor@192.168.1.17:5432/smc")
+# database_data = pd.read_sql("SELECT * FROM tbl_siteeval WHERE login_agency = 'VCWPD' AND created_date = '2019-08-13 09:38:49' ",eng)
+# user_data = pd.read_excel("C:\\Users\\GisUser\\Desktop\\user_data.xls")
+# user_data.columns = map(str.lower, user_data.columns)
 
 
-for col in user_data.columns:
-    if "date" in col:
-        user_data[col].fillna(pd.Timestamp('1950/01/01'),inplace =True)
+# for col in user_data.columns:
+#     if "date" in col:
+#         user_data[col].fillna(pd.Timestamp('1950/01/01'),inplace =True)
 
-#This function is to compare the two data frame regardless of their sizes
-#Adjust the directory to see the output files        
-primarykeys = ['stationcode','drawcode','programcode','agencycode','evaldate']
+# #This function is to compare the two data frame regardless of their sizes
+# #Adjust the directory to see the output files        
+# primarykeys = ['stationcode','drawcode','programcode','agencycode','evaldate']
 
-def compare_and_highlight(df1,df2,primarykeys):
-    mergingcols = primarykeys
+def compare_and_highlight(df_origin,df_modified,pkey_columns):
+    mergingcols = pkey_columns
     #database_data.drop(columns = [col for col in database_data.columns if col not in user_data.columns],inplace = True)
 
-    merged = pd.merge(
-                df1, 
-                df2,
+    merged_df = pd.merge(
+                df_origin, 
+                df_modified,
                 on = mergingcols,
                 how = 'outer',
                 suffixes = ('_old','')
                 )
 
-    merged['change_type'] = merged.apply(
-            lambda x: 
-                "addition" 
-                # if all of the "old" column values are null, then the user added a new record
-                if all(
-                        [pd.isnull(x[f'{col}_old']) for col in df1.columns if col not in mergingcols ]) 
-                else "deletion"
-                # if all of the "new" columns have null values, then the user deleted a record
-                if all(
-                    [pd.isnull(x[f'{col}']) for col in df1.columns if col not in mergingcols  ]
-                    )
-                else "modification"
-                # if any of the old column values are different from the new columns values, it is a modified record
-                if any(
-                        [x[f'{col}'] != x[f'{col}_old'] for col in df1.columns if col not in mergingcols ]
+    merged_df['change_type'] = merged_df.apply(
+            lambda x:
+            "addition"
+            # if all of the "old" column values are null, then the user added a new record
+            if all(
+                [pd.isnull(x[f'{col}_old']) for col in df_origin.columns if col not in pkey_columns]
+            )
+            else "deletion"
+            # if all of the "new" columns have null values, then the user deleted a record
+            if all(
+                [pd.isnull(x[f'{col}']) for col in df_origin.columns if col  not in pkey_columns ]
+            )
+            else "modification"
+            # if any of the old column values are different from the new columns values, it is a modified record
+            if not all(
+                [
+                    ( 
+                        (
+                            (x[f'{col}'] == x[f'{col}_old']) if col != 'mdl' else (float(x[f'{col}']) == float(x[f'{col}_old']))
                         )
-        # if it doesn't meet any above case, there is no change and we return a Nonetype object
+                        | 
+                        (
+                            all(
+                                map(
+                                    #pd.isnull,
+                                    # Treats NULLs and empty strings the same, since they are mixed around in our database
+                                    lambda value: pd.isnull(value) or value == '', 
+                                    [x[f'{col}'], x[f'{col}_old']] 
+                                ) 
+                            ) 
+                        ) 
+                    )
+                    for col in df_origin.columns if col not in pkey_columns 
+                ]
+            )
+            # if it doesn't meet any above case, there is no change and we return a Nonetype object
             else None 
 
-        # don't forget that axis = 1 since the args passed to the lambda function are rows of the dataframe
-            , axis = 1 
-            )
+            # don't forget that axis = 1 since the args passed to the lambda function are rows of the dataframe
+            , axis = 1
+        )
 
     #Extract the information that we want - either the records are added, modified, or deleted
     #primarykeys are in 2 not in 1
-    add_records =  merged[merged['change_type'] == "addition"]
+    add_records =  merged_df[merged_df['change_type'] == "addition"]
     #primarykeys are in both df, but there are some modifications
-    modified_records = merged[merged['change_type'] == "modification"]
+    modified_records = merged_df[merged_df['change_type'] == "modification"]
     #primarykeys are in 1 not in 2
-    delete_records = merged[merged['change_type'] == "deletion"]
+    delete_records = merged_df[merged_df['change_type'] == "deletion"]
 
     modified_records.replace("", np.NaN, inplace = True)
 
@@ -128,7 +147,7 @@ def compare_and_highlight(df1,df2,primarykeys):
 
     # after grabbing coordinates of what we need, drop that "same" column
         modified_records.drop(col, axis = 1, inplace = True)
-    writer = pd.ExcelWriter("P:\\PartTimers\\DuyNguyen\\Projects\\GeneralDiffCheckMaster\\General-Diff-Check\\output\\MODIFIED.xlsx", engine = 'xlsxwriter')
+    writer = pd.ExcelWriter("MODIFIED.xlsx", engine = 'xlsxwriter')
 
     modified_records.to_excel(writer, sheet_name = "modified",index = False)
 
@@ -166,3 +185,6 @@ def compare_and_highlight(df1,df2,primarykeys):
 #writer2 = pd.ExcelWriter(directory2, engine = 'xlsxwriter')
 #add_records.to_excel(writer2,index = False)
 #writer2.save()
+df_origin = pd.read_excel('Test1.xlsx')
+df_modified = pd.read_excel('Test2.xlsx')
+compare_and_highlight(df_origin,df_modified,'name')
